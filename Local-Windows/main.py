03e9c325e7688fb1ee23f8e964c7ad3148ac9b46
Arcelibs <s12873514@gmail.com>
2023-12-26 10:18:26 +0800

    修改並分類資料夾

---
+++ Local-Windows/main.py
@@ -0,0 +1,101 @@
+import requests
+import json
+import os
+import subprocess
+import time
+import whisper
+from faster_whisper import WhisperModel
+
+
+# 初始化 Faster Whisper 模型
+model = WhisperModel("large-v2", device="cpu", compute_type="int8_float16")  # 使用适合您硬件的模型大小和设备
+
+
+def call_gemini_api(input_text):
+    url = f"https://palm-proxy.arcelibs.com/v1beta/models/gemini-pro:generateContent?key=AIzaSyC03RJ0NEzidKXjeTZQqYVjjT5NWs4dYpw"
+    headers = {'Content-Type': 'application/json'}
+    formatted_input = f"請你必須將下列語句翻譯成流暢的繁體中文:\n{input_text}，"
+    data = json.dumps({"contents":[{"parts":[{"text": formatted_input}]}]})
+
+    response = requests.post(url, headers=headers, data=data)
+    if response.status_code == 200:
+        response_data = response.json()
+        #print("Response Data:", response_data)  # 调试：打印响应数据
+        try:
+            return response_data['candidates'][0]['content']['parts'][0]['text']
+        except KeyError:
+            print("KeyError: 'candidates' not found in response.")
+            return None
+    else:
+        print(f"错误: {response.status_code}")
+        return None
+
+
+# 使用 yt-dlp 获取 YouTube 媒体流地址
+def get_stream_url(youtube_url):
+    command = ["yt-dlp", "-g", youtube_url]
+    process = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+    if process.returncode != 0:
+        print(f"错误: 无法获取流地址。详情：{process.stderr}")
+        return None
+    return process.stdout.strip()
+
+# 录制音频的函数
+def record_audio(stream_url, duration, output_filename):
+    try:
+        command = [
+            "ffmpeg", "-i", stream_url, "-t", str(duration),
+            "-ac", "1", "-ar", "16000", "-y", output_filename
+        ]
+        subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, check=True)
+    except subprocess.CalledProcessError as e:
+        print(f"错误: 无法录制音频。详情：{e.stderr.decode()}")
+        return False
+    return True
+
+# 转写音频的函数
+def transcribe_audio(file_path, language="japanese"):
+    try:
+        # 使用 Faster Whisper 进行转录
+        segments, info = model.transcribe(file_path)
+        # 合并段落以形成完整文本
+        text = " ".join([seg.text for seg in segments])
+        return text
+    except Exception as e:
+        print(f"错误: 无法转写音频。详情：{e}")
+        return None
+
+
+
+# 主流程
+def main(youtube_url, segment_duration, total_duration):
+    stream_url = get_stream_url(youtube_url)
+    if not stream_url:
+        print("无法获取 YouTube 直播流地址。")
+        return
+
+    for segment in range(0, total_duration, segment_duration):
+        filename = f"output{segment}.wav"
+        if record_audio(stream_url, segment_duration, filename):
+            text = transcribe_audio(filename)
+            if text:
+                translated_text = call_gemini_api(text)
+                if translated_text:
+                    #print(f"Segment {segment}: {translated_text}")
+                    print(f"{translated_text}")
+                else:
+                    print(f"无法翻译段落 {segment}。")
+            else:
+                print(f"无法转录段落 {segment}。")
+            os.remove(filename)  # 清理：删除音频文件
+        else:
+            print(f"录制段落 {segment} 失败。")
+        time.sleep(segment_duration)
+
+# 配置参数
+YOUTUBE_URL = "https://www.youtube.com/watch?v=SvoZtc_Z9ZA"  # YouTube 直播页面 URL
+SEGMENT_DURATION = 10  # 每个音频片段的长度，单位：秒
+TOTAL_DURATION = 6000   # 总录制时间，单位：秒
+
+if __name__ == "__main__":
+    main(YOUTUBE_URL, SEGMENT_DURATION, TOTAL_DURATION)
--- README.md
+++ README.md
@@ -13,48 +13,11 @@ GPT流派的Youtube直播流切片翻譯(半即時)
 
 最後用Whisper去讀取wav轉文字印出，印出的文字再翻譯
 
-## 操作環境
-Google Colab With T4 GPU(Free)
+## Colab版本
+運用於Google Colab使用，其他ipynb環境無測試
 
-顯卡也不是不行，但是方便多裝置連網使用
-
-## 版本差異
-### V1版本使用DeepL翻譯
-
-翻譯速度:最快 / 翻譯語意:接近 / 翻譯語言:簡體中文
-
-(需要用到DeepL的API，自己想辦法取得，不建議再翻譯)
-
-### V2版本使用OpenAI GPT3.5翻譯
-
-翻譯速度:中 / 翻譯語意:接近 / 翻譯語言:繁體中文
-
-不建議使用，因為OpenAI API太貴了，看五分鐘要0.01美金
-
-### V30版本使用Google Gemini Pro翻譯
-
-翻譯速度:快 / 翻譯語意:接近 / 翻譯語言:繁體中文
-
-改用large-V2模型
-
-改用Faster-Whisper框架，大幅降低VRAM需求
-
-改用Google LLM Gemini Pro API，優點就是不用錢又快
-
-
-## 相關api取得方法
-DeepL API: 需要海外的信用卡才能拿到免費API，免費額度500000字母/月
-
-OpenAI GPT3.5: 註冊OpenAI帳號申請，免費額度用完需要儲值
-
-Gemini Pro API: 免費申請，但僅限美國地區使用，美國以外需要代理
-
-代理方法:
-
-使用CF Worker: https://zhile.io/2023/12/24/gemini-pro-proxy.html
-
-使用Vercel: https://simonmy.com/posts/%E4%BD%BF%E7%94%A8vercel%E5%8F%8D%E5%90%91%E4%BB%A3%E7%90%86google-palm-api.html
-
+## Local-Windows版本
+運用於Windows環境使用
 
 
 
